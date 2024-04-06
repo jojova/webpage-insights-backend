3b3b9c78f957fd60281a991146b2ab02fc5cd8f9
Gokul <gokulkrishna.gk32@gmail.com>
2024-04-07 00:11:03 +0530

    Added routes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from webchat.view import router as webchat_router
 from summarize.view import router as summarize_router
 from csvchat.view import router as csv_chat_router
 from scrape.view import router as scrape_router
+from image_caption.view import router as image_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
@@ -30,6 +31,7 @@ app.include_router(webchat_router, tags=["Webchat"], prefix="/webchat")
 app.include_router(summarize_router, tags=["Summarize"], prefix="/summarize")
 app.include_router(csv_chat_router,tags=["CSV"], prefix="/csv")
 app.include_router(scrape_router,tags=["Scrape"], prefix="/scrape")
+app.include_router(image_router,tags=["Caption"], prefix="/caption")
 
 @app.get("/", tags=["Test"])
 async def test_response():
